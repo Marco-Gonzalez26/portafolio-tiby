d18ace8c645b7803a0bbdb0a47c60099f18cf05f
Marco Gonzalez <magp2606@gmail.com>
2024-01-22 00:31:59 -0500

    Fix overlapping on mobile

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -71,7 +71,7 @@ export const reviewsData: Review[] = [
     author: 'Ricardo González',
     rate: 5,
     review:
-      'Tibisay es realmente excepcional. Su habilidad para gestionar tareas utilizando Excel es impresionante la cual mejora significativamente la eficiencia del consultorio. Además, su atención al paciente es ejemplar; brinda un trato amable y profesional que contribuye a la satisfacción de los pacientes. Su destreza técnica y calidad en el servicio hacen que sea una pieza invaluable en el equipo médico.',
+      'Tibisay es realmente amigable y atenta. Su habilidad para gestionar tareas utilizando Excel es impresionante la cual mejora significativamente la eficiencia del consultorio. Además, su atención al paciente es ejemplar; brinda un trato amable y profesional que contribuye a la satisfacción de los pacientes. Su destreza técnica y calidad en el servicio hacen que sea una pieza invaluable en el equipo médico.',
     charge: 'Médico Cardiólogo'
   }
 ]
